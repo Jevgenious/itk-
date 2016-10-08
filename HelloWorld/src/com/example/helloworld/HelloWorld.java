9c0551795dc83b68cb9fe98d1f5553ec256d34aa
root <root@RS>
2016-10-08 11:36:01 +0300

    test2

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by jevgenious on 10/6/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Tere maailm!");
+    }
+}
